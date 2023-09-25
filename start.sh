bcfdbf0a1b558e2eaea9480a1cfe811f4260e5ae
Teotimo Garcia <teo@rekeepit.com>
2023-09-25 09:14:30 -0700

    chore: Debug logs

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo PROMETHEUS_BEARER_TOKEN is "${PROMETHEUS_BEARER_TOKEN}"
+
 # Replace the placeholder with the actual bearer token from the environment variable
 sed -i "s/PROMETHEUS_BEARER_TOKEN/${PROMETHEUS_BEARER_TOKEN}/g" /app/prometheus.yml
+
+cat /app/prometheus.yml
 
 # Start Prometheus
 /app/bin/prometheus --config.file=$HOME/prometheus.yml --web.enable-admin-api --web.listen-address=:$PORT
